c1d0f2c81caede62b720755426dbd992cf34e2a9
Rifaldi01 <rifaldipratama2004@gmail.com>
2021-08-11 11:17:45 -1100

    pkl

---
+++ app/Http/Controllers/MultimediaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MultimediaController extends Controller
+{
+    public function Detail(){
+        return view('detail');
+    }
+}
